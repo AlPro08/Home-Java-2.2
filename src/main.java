339865fad4ff239bd180b949d8fc83103cecc0f1
Aleksandr Prokhorov <proxor1186@gmail.com>
2022-06-15 23:09:50 +0300

    popolnenie cheta

---
+++ src/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+
+        int accountBallance = 205; // денег на счету у пользователя
+        int replacement = 1001; // сумма пополнения счета
+        int bonuce; // переменная для расчета бонусов
+
+        if (replacement > 1000) {
+            bonuce = replacement / 100;
+        } else {
+            bonuce = 0;
+        }
+        int lastBallance = accountBallance + replacement + bonuce;
+        System.out.println("бонус равен " + bonuce);
+        System.out.println("баланс равен " + lastBallance);
+
+            }
+}
